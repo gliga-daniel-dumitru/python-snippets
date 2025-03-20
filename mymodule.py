cb4ad7d482d717bdd7b377e26c542742a986dc06
Gliga Daniel Dumitru <gliga.dumitru@3pillarglobal.com>
2025-03-20 15:14:43 +0200

    chore: update logic

--- mymodule.py
+++
@@ -1,5 +0,0 @@
-def gret(name):
-    print(f"Hello {name}")
-
-def gret2(name):
-    print(f"Hello {name}")
--- script.py
+++ script.py
@@ -1,15 +1,2 @@
 #This is a comment
 print("Hello, World!") #This is a comment
-
-#This is a comment
-#written in
-#more than just one line
-print("Hello, World!")
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
-
